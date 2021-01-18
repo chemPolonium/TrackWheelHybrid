34b5820979563239229f9ad209afe921f6462e0f
chemPolonium <li.boyuan@outlook.com>
2021-01-18 19:50:33 +0800

    some check on in and out to left and right

--- hybridforce.m
+++ hybridforce.m
@@ -1,42 +1,50 @@
-function [Fx,Fy,Mz] = hybridforce(vx,vy,omegaz,fx1,fx2,steer1,steer2)
+function [Fx,Fy,Mz] = hybridforce(vx,vy,omegaz,omegal,omegar,fx1,fx2,steer1,steer2)
 % HYBRIDFORCE  Get the total force of the track-tire hybrid vehicle.
 %   HYBRID(vx,vy,omegaz,fx1,fx2)
 %   the hybrid force can calculate the force of the tracks and tires.
 
+% omegaz 车身旋转速度
+% omegar 外侧履带轮转动速度
+% omegal 内测履带轮转动速度
+% fx1 前轮驱动力
+% fx2 后轮驱动力
+% steer1 前轮转角
+% steer2 后轮转角
+
 cx = 0; % X方向（车辆横向lateral）上质心到几何中心距离，质心在几何中心左时为正
 cy = 0; % Y方向（车辆纵向longitudinal）上质心到几何中心距离，质心在几何中心右时为正
 
-[Fxr,Fyr,MLr,Mrr,Fxl,Fyl,MLl,Mrl] = trackedforce;
+[Fxl,Fyl,MLl,Mrl,Fxr,Fyr,MLr,Mrr] = trackedforce;
 [Ftfx,Ftfy,Mztf,Ftrx,Ftry,Mztr] = tireforce;
 
-Fx = Fxr+Fxl+Ftfx+Ftrx;
-Fy = Fyr+Fyl+Ftfy+Ftry;
-Mz = MLr+Mrr+MLl+Mrl+Mztf+Mztr;
+Fx = Fxl+Fxr+Ftfx+Ftrx;
+Fy = Fyl+Fyr+Ftfy+Ftry;
+Mz = MLl+Mrl+MLr+Mrr+Mztf+Mztr;
 
-    function [Fxl,Fyl,MLl,Mrl,Fxr,Fyr,MLr,Mrr] = trackedforce
+    function [Fxl,Fyl,MLl,Mrl,Fxr,Fyr,MLr,Mrr] = trackedforcegeocord
         
         % using the coordinate system in Wong's book.
         % where the heading direction is y,
         % the lateral is x,
         % z = cross(x,y) to the sky.
         
+        % 认为履带外侧为右（书上图片）
         % 履带受力
         % Rx = 10; % R''
         B = 3; % 两条履带中心线之间的距离
         l = 2; % 履带长度
         % so = 0.5; % Y方向（车辆纵向）上质心到旋转中心距离
-        omegaz = 0.1; % 车身旋转速度
         r = 0.3; % 履带轮半径
-        omegao = 2; % 外侧履带轮转动速度
-        omegai = 1.4; % 内测履带轮转动速度
         b = 0.3; % 履带宽度
-        sigmao = 1; % 外侧履带法向压力（压强）
-        sigmai = 1; % 内侧履带法向压力（压强）
+        sigmar = 1; % 右侧履带法向压力（压强）
+        sigmal = 1; % 左侧履带法向压力（压强）
         mu = 0.5; % 履带和地面的摩擦系数
         K = 0.2;
         
         Rx = vx/omegaz + cx;
         so = vy/omegaz;
+        
+        % 也许可以使用fillmissing改写
         
         x1 = linspace(-b/2,b/2,10);
         y1 = linspace(-l/2-(so-cy),l/2-(so-cy),50)';
@@ -44,31 +52,36 @@ Mz = MLr+Mrr+MLl+Mrl+Mztf+Mztr;
         x2 = linspace(-b/2,b/2,10);
         y2 = linspace(-l/2-(so-cy),l/2-(so-cy),50)';
         
-        jxr = (Rx+B/2+cx+x1).*(cos((l/2+cy-so-y1)*omegaz/r/omegao)-1)-y1.*sin((l/2+cy-so-y1)*omegaz/r/omegao);
-        jyr = (Rx+B/2+cx+x1).*sin((l/2+cy-so-y1)*omegaz/r/omegao)-(l/2+cy-so)+y1.*cos((l/2+cy-so-y1)*omegaz/r/omegao);
+        jxr = (Rx+B/2+cx+x1).*(cos((l/2+cy-so-y1)*omegaz/r/omegar)-1)-y1.*sin((l/2+cy-so-y1)*omegaz/r/omegar);
+        jyr = (Rx+B/2+cx+x1).*sin((l/2+cy-so-y1)*omegaz/r/omegar)-(l/2+cy-so)+y1.*cos((l/2+cy-so-y1)*omegaz/r/omegar);
         jr = hypot(jxr,jyr);
         
-        vjyr = (Rx+B/2+cx+x1)*omegaz-r*omegao;
+        vjyr = (Rx+B/2+cx+x1)*omegaz-r*omegar;
         vjxr = -y1*omegaz;
         
         % vjr = hypot(jxr,jyr);
         % sindeltar = vjyr./vjr;
         % cosdeltar = vjxr./vjr;
         
+        % 使用atan2可以规避掉0除0的问题
+        % 其物理意义在于，履带刚接触地面时，对土壤位移为0
+        % 计算三角函数时会出现除0的情况
+        % 但是实际上此时力也是0，不会有影响
+        % 直接删掉最后一行可以达到一样的效果
         deltar = atan2(vjyr,vjxr);
         sindeltar = sin(deltar);
         cosdeltar = cos(deltar);
         
-        dFxr = -sigmao*mu*(1-exp(-jr/K)).*cosdeltar;
-        dFyr = -sigmao*mu*(1-exp(-jr/K)).*sindeltar;
-        dMLr = -(B/2+x1)*sigmao*mu.*(1-exp(-jr/K)).*sindeltar;
-        dMrr = -y1*sigmao*mu.*(1-exp(-jr/K)).*cosdeltar;
+        dFxr = -sigmar*mu*(1-exp(-jr/K)).*cosdeltar;
+        dFyr = -sigmar*mu*(1-exp(-jr/K)).*sindeltar;
+        dMLr = -(B/2+x1)*sigmar*mu.*(1-exp(-jr/K)).*sindeltar;
+        dMrr = -y1*sigmar*mu.*(1-exp(-jr/K)).*cosdeltar;
         
-        jxl = (Rx-B/2+cx+x2).*(cos((l/2+cy-so-y2)*omegaz/r/omegai)-1)-y2.*sin((l/2+cy-so-y2)*omegaz/r/omegai);
-        jyl = (Rx-B/2+cx+x2).*sin((l/2+cy-so-y2)*omegaz/r/omegai)-(l/2+cy-so)+y2.*cos((l/2+cy-so-y2)*omegaz/r/omegai);
+        jxl = (Rx-B/2+cx+x2).*(cos((l/2+cy-so-y2)*omegaz/r/omegal)-1)-y2.*sin((l/2+cy-so-y2)*omegaz/r/omegal);
+        jyl = (Rx-B/2+cx+x2).*sin((l/2+cy-so-y2)*omegaz/r/omegal)-(l/2+cy-so)+y2.*cos((l/2+cy-so-y2)*omegaz/r/omegal);
         jl = hypot(jxl,jyl);
         
-        vjyl = (Rx-B/2+cx+x2)*omegaz-r*omegai;
+        vjyl = (Rx-B/2+cx+x2)*omegaz-r*omegal;
         vjxl = -y2*omegaz;
         
         deltal = atan2(vjyl,vjxl);
@@ -78,10 +91,10 @@ Mz = MLr+Mrr+MLl+Mrl+Mztf+Mztr;
         % sindelta2 = vjyl./vjl;
         % cosdelta2 = vjxl./vjl;
         
-        dFxl = -sigmai*mu*(1-exp(-jl/K)).*cosdeltal;
-        dFyl = -sigmai*mu*(1-exp(-jl/K)).*sindeltal;
-        dMLl = -(B/2-x2)*sigmai*mu.*(1-exp(-jl/K)).*sindeltal;
-        dMrl = -y2*sigmai*mu.*(1-exp(-jl/K)).*cosdeltal;
+        dFxl = -sigmal*mu*(1-exp(-jl/K)).*cosdeltal;
+        dFyl = -sigmal*mu*(1-exp(-jl/K)).*sindeltal;
+        dMLl = -(B/2-x2)*sigmal*mu.*(1-exp(-jl/K)).*sindeltal;
+        dMrl = -y2*sigmal*mu.*(1-exp(-jl/K)).*cosdeltal;
         
         duotrapz = @(d) trapz(x1,trapz(y1,d));
         
@@ -102,6 +115,11 @@ Mz = MLr+Mrr+MLl+Mrl+Mztf+Mztr;
         % 需要把中间一部分运算创建成一个函数
         % Fxo = integral2(@(x,y) getdFxo(x,y),-b/2,b/2,-l/2-(so-cy),l/2-(so-cy))
         
+    end
+
+    function [Fxl,Fyl,MLl,Mrl,Fxr,Fyr,MLr,Mrr] = trackedforce
+        [Fxl,Fyl,MLl,Mrl,Fxr,Fyr,MLr,Mrr] = trackedforcegeocord;
+        [Fxl,Fyl,Fxr,Fyr] = deal(Fyl,-Fxl,Fyr,-Fxr);
     end
 
     function Fy = fialatireforce(talpha,Fx)
@@ -126,7 +144,7 @@ Mz = MLr+Mrr+MLl+Mrl+Mztf+Mztr;
 
     function [Fx,Fy,Mz] = singletireforce(l,delta,FxTire)
         % delta: steer angle
-        talpha = (vy+omegaz*l)/vx;
+        talpha = tan(atan2(vy+omegaz*l,vx));
         FyTire = fialatireforce(talpha,FxTire);
         Fx = FxTire*cos(delta)-FyTire*sin(delta);
         Fy = FyTire*cos(delta)+FxTire*sin(delta);
